dd172e2880aaf9a9cdfb138a9ddd305130d230cb
Arun Mahendrakar <Arun_Mahendrakar@Dell.com>
2021-03-17 08:22:19 +0530

    chapter 11

--- 11Assignments.py
+++ 11Assignments.py
@@ -1,2 +1,18 @@
 # Assignments, Expressions, and Prints
 
+# tuple assignment positional
+one, two = '1', '2'
+print(one)
+print(two)
+
+# list assignment positional
+[spam, ham] = ['yum', 'YUM']
+print(spam)
+print(ham)
+
+# string length should match the number of variables
+# else throws 'too many values to unpack (expected 4)' error
+a, b, c, d = 'spam' # Sequence assignment, generalized
+a, b, c, d # shows ('s', 'p', 'a', 'm')
+e, *f = 'spam' # Extended sequence unpacking
+e, f # shows ('s', ['p', 'a', 'm'])
