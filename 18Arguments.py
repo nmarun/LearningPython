81ba7a39ecd0aa3e08d246d50207193391e9f199
Arun Mahendrakar <Arun_Mahendrakar@Dell.com>
2021-03-26 07:10:40 +0530

    arguments

--- 18Arguments.py
+++ 18Arguments.py
@@ -1,3 +1,10 @@
 # Arguments
 
+def sum_all(*args): # *args converts arbitrary number of inputs into a tuple
+    total = 0
+    for val in args:
+        total += val
+    return total
 
+arguments = 1, 2, 3, 4
+sum_all(*arguments) # sending without the * would make the argument as a single tuple; 
