ba3e17ab79fdfa3b7f35b5dbf9e256b71dcc6411
Arun Mahendrakar <Arun_Mahendrakar@Dell.com>
2021-03-27 06:43:12 +0530

    chapter 19 finished

--- 19AdvancedFunctionTopics.py
+++ 19AdvancedFunctionTopics.py
@@ -1,3 +1,16 @@
 # Advanced Function Topics
 
+# Recursion
+def mysum(L):
+    if not L:
+        return 0
+    else:
+        return L[0] + mysum(L[1:]) # Call myself recursively
 
+list = [1, 2, 3]
+mysum(list)
+
+from functools import reduce
+
+reduce((lambda x, y: x + y), [1, 2, 3, 4])
+reduce((lambda x, y: x * y), [1, 2, 3, 4])
---
+++ 20ComprehensionsGenerations.py
@@ -0,0 +1,3 @@
+# Comprehensions and Generations
+
+
