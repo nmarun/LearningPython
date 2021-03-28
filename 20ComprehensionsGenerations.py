8d831399edcc859da424274e8baf9c2a6580b215
Arun Mahendrakar <Arun_Mahendrakar@Dell.com>
2021-03-28 07:12:06 +0530

    chapter 20 finished

--- 20ComprehensionsGenerations.py
+++ 20ComprehensionsGenerations.py
@@ -1,3 +1,11 @@
 # Comprehensions and Generations
 
+#scrambling
+name = 'Arun'
 
+def scramble(seq):
+    return [seq[i:] + seq[:i] for i in range(len(seq))]
+
+scramble(name)
+items = ['laptop', 'book', 'phone']
+scramble(items)
